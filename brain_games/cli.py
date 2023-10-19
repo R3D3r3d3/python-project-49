f9a0991775972f7cefd038a8a3672a4451b939c7
R3D3 <spoked-medals-0@icloud.com>
2023-10-19 17:46:31 +0300

    add welcome_user() function and modified related cli.py brain_games.py, poetry.lock and pyproject.toml

---
+++ brain_games/cli.py
@@ -0,0 +1,11 @@
+import prompt
+
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    return f"Hello, {name}!"
+
+def main():
+    print(welcome_user())
+
+if __name__ == '__main__':
+    main()
